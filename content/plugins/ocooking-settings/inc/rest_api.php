d4a249c6f794bb8ed5aa0b075a563d02c0050b8a
ChristopheOclock <christophe@oclock.io>
2020-02-17 11:46:52 +0100

    Début de l'ajout du champ thumbnail

--- content/plugins/ocooking-settings/inc/rest_api.php
+++ content/plugins/ocooking-settings/inc/rest_api.php
@@ -5,6 +5,7 @@ class oCookingRestApi
     public function __construct()
     {
         add_action('rest_api_init', [$this, 'authorField']);
+        add_action('rest_api_init', [$this, 'thumbnailField']);
     }
 
     public function authorField()
@@ -45,5 +46,44 @@ class oCookingRestApi
             'id' => $object['author'],
             'name' => get_the_author_meta('nickname', $object['author'])
         ];
+    }
+
+    public function thumbnailField()
+    {
+        // Je souhaite étendre(=modifier) le retour de la REST Api de WP
+        register_rest_field(
+            // Type(s) de contenu(s) que je souhaite modifier
+            // (ici les articles & les recettes)
+            ['post', 'recipe'],
+            // Nom du champ/clé à ajouter
+            // (si la clé existe, elle est modifiée)
+            'thumbnail',
+            // La liste des méthodes à executer pour
+            // traiter mon champ
+            [
+                // fonction à appeler lors d'un GET
+                'get_callback' => [$this, 'getThumbnailDetails'],
+                //  fonction à appeler lors d'un POST
+                'update_callback' => null,
+                // Structure de ma donnée
+                'schema' => null
+            ]
+        );
+    }
+
+    /**
+     * Callback appelé lors d'un GET (ex: wp/v2/posts) pour modifier la clé "author"
+     *
+     * @param array $object Le post courant (tableau avec toutes les données du contenu AVANT la modification en cours)
+     * @param string $field_name Le nom du champ/clé ciblé (ou du nouveau champ/clé créé).
+     * @param WP_REST_Request $request Requête courante
+     *
+     * @return void
+     */
+    public function getThumbnailDetails($object, $field_name, $request)
+    {
+        // https://developer.wordpress.org/reference/functions/wp_get_attachment_image_src/
+
+        return 'Le détail de mon thumbnail...';
     }
 }
